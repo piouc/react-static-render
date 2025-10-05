14208e6ee4cddd04345a387ae737d0235cde3f5c
piou <piou@hotmail.co.jp>
2025-10-06 08:00:59 +0900

    Remove not used files

--- test-app/src/components/button.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Button = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <button>
-      {children} - Version 3 TEST
-    </button>
-  )
-}
--- test-app/src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-  return (
-    <footer>
-      <p>© 2024 My App - Version 2 UPDATED</p>
-    </footer>
-  )
-}
